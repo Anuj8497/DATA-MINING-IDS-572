1dec7c3071f86c59c65a8d5d4dcb9f5c3e0b5630
Anuj Chanchlani <57273902+Anuj8497@users.noreply.github.com>
2020-02-12 00:16:33 -0600

    Update ques5.R

--- ques5.R
+++ ques5.R
@@ -10,7 +10,9 @@ library(ggplot2) # for plots
 library(ggthemes)
 library(feather)
 dataset = read.csv("gun_deaths.csv")
+dataset = na.omit(dataset)
 dummy = dataset
+
 # a 
 
 # For determining the no of deaths by month we first need to convert the months into qualitative.
@@ -19,11 +21,10 @@ dataset$month <- as.factor(dataset$month)
 new <- data.frame(summary(dataset$month))
 
 #output
- 
+
 #  1    2    3    4    5    6    7    8    9   10   11   12 
 # 8273 7093 8289 8455 8669 8677 8989 8783 8508 8406 8243 8413 
 
-dummy$label[dummy$month==1]<-"Jan"
 
 # b
 
@@ -51,22 +52,13 @@ dev.off()
 
 # c
 
-y <- data.frame(dataset$id, dataset$intent)
-summary(y)
+tabb <- table(dataset$intent)
+tabb<-sort(tabb,decreasing = TRUE)
+# Let's add margins for a better picture
+ptab <-addmargins(prop.table(tabb))
+addmargins(prop.table(tabb))
+barplot(tabb, main = "Bar Plot", col=c("cadetblue", "gold"))
 
-z <- data.frame(aggregate(dataset$id ~ dataset$intent, y, sum))
-
-var <- z[order(-z$dataset.id),c(1,2)]
-
-ggplot(data = var, mapping = aes(x =var$dataset.intent, y = var$dataset.id))+geom_bar(stat ="identity" , fill = "red")
-
-# plot with unarranged bars. 
-ggplot(var,aes(var$dataset.intent,var$dataset.id))+geom_bar(stat = "identity", fill = "red")
-
-# using order function with -ve sign we arrange the bars from highest to lowest. But the x ticks gets
-# replaced by numbering 1,2,3,4 since we used the order function.
-
-ggplot(var,aes(order(-var$dataset.intent),var$dataset.id))+geom_bar(stat = "identity", fill = "red")
 
 # d
 
@@ -76,29 +68,32 @@ boxplot(dataset$age ~dataset$sex, data=dataset, main="Age of gun death victims b
 female <- dataset[ which(dataset$sex=='F'), ]
 
 # now lets calculate average age of female gun deaths
-female <- na.omit(female)
 ans  <- mean(female$age)
 
 summary(dataset$education)
 
 # e 
-
+  
 ans <- count(dataset[ which(dataset$education=='HS/GED' & dataset$race =='White' & dataset$year == 2012 & dataset$sex == "M"), ])
+plot(ans)
 
-# 7912 were the no of deaths in 2012 who were male, white with atleast a high school education.
+# 7794 were the no of deaths in 2012 who were male, white with atleast a high school education.
 
 # f
 
-# creating a new variable named seaso storing the season associated with each month
+# creating a new variable named season storing the season associated with each month
 
 dataset$season[dataset$label=="Jan" | dataset$label == "Feb" | dataset$label == "Mar"]<-"Winter"
 dataset$season[dataset$label=="Apr" | dataset$label == "May" | dataset$label == "Jun"]<-"Spring"
 dataset$season[dataset$label=="Jul" | dataset$label == "Aug" | dataset$label == "Sept"]<-"Summer"
 dataset$season[dataset$label=="Oct" | dataset$label == "Nov" | dataset$label == "Dec"]<-"Fall"
 
-max(table(dataset$season))
 
-# from the table it is clear that Summer is the season with most gun deaths.
+table <- table(dataset$season)
+barplot(table, main = "Bar Plot", col=c("cadetblue", "gold"))
+
+
+# from the table & plot it is clear that Summer is the season with most gun deaths.
 
 # g
 
@@ -108,44 +103,44 @@ max(table(dataset$season))
 # In case of Whites
 
 count(dataset[ which(dataset$intent=='Suicide' & dataset$race =='White'), ])
-#55372
+#56415
 count(dataset[ which(dataset$intent=='Homicide' & dataset$race =='White'), ])
-#9147
+#8293
 
-55372/100798 # i.e. 54.9%
-9147/100798 # i.e 9.07 %
+56415/98015 # i.e. 57.5%
+8293/98015 # i.e 8.4 %
 
-# Hence in case of white gun violences are more likely to happen due to suicide.
+# Hence in case of white gun violences involving suicide are more likely to happen.  
 
 # In case of Blacks
 
 count(dataset[ which(dataset$intent=='Suicide' & dataset$race =='Black'), ])
 
-# 3332
+# 3285
 
 count(dataset[ which(dataset$intent=='Homicide' & dataset$race =='Black'), ])
 
-# 19510
+# 18956
 
-3332/100798 # i.e. 3.3%
-19510/100798 # i.e 19.35%
+3285/98015 # i.e. 3.35%
+19510/98015 # i.e 19.91%
 
-# Hence in case of blacks gun violences are more likely to happen due to homicide
+# Hence in case of blacks gun violences involving homicide are more likely to happen. 
 
 # In case of Hispanics
 
 count(dataset[ which(dataset$intent=='Suicide' & dataset$race =='Hispanic'), ])
 
-# 3171
+# 3120
 
 count(dataset[ which(dataset$intent=='Homicide' & dataset$race =='Hispanic'), ])
 
-# 5634
+# 5269
 
-3171/100798 # i.e. 3.1%
-5634/100798 # i.e 5.5%
+3120/98015 # i.e. 3.1%
+5634/98015 # i.e 5.7%
 
-# Hence in case of Hispanic gun violences are more likely to happen due to homicide
+# Hence in case of Hispanic gun violences involving homicide are more likely to happen. 
 
 # h
 
@@ -156,8 +151,57 @@ dataset$police <- as.factor(dataset$police)
 # Now in order to check whether the police involved gun deaths are significantly different from other gun
 # deaths, we will use chi square test. The reason being since both intent and police are factors.
 
-pol <- dataset[dataset$police == 1,]
-no_pol <- dataset[dataset$police == 0,]
 
-stab<- table(pol$police, no_pol$police)
-chisq.test(stab) 
+table(dataset$police)
+
+#  0     1 
+# 97996    19
+# The police invloved gun deaths are very less as compared to the non-police involvement.
+
+# Accesing relationships between police involvement and other variables.
+
+# with intent
+
+table(dataset$police,dataset$intent)
+
+# from the table we see all the gundeaths in which police were involved are homicide.Same is not the case when police were not involved.
+
+# with year
+
+table(dataset$year,dataset$police)
+
+# from this we can see that from 2012 to 2014 non police involvement gun violences were more than police involved
+# gun violences.
+
+# with sex
+
+table(dataset$sex,dataset$police)
+
+# From this we get the information that all police invloved gun deaths involved males.
+# Whereas in non police involved gun deaths involving males was almost six times more as compared to females 
+
+table(dataset$race,dataset$police)
+
+# In case of police involvement of non-police involvement, majority of victims belonged to White race.
+# with education
+
+table(dataset$education,dataset$police)
+
+# In both the cases, majority of the victims were atleast High School graduates.
+
+# with age
+
+aggregate(dataset$age, by = list(dataset$police) ,FUN = mean)
+
+# In police involved gun deaths the average age of victims is ~ 36 and for non police invloved is ~ 44.  
+
+# with place
+
+table(dataset$place,dataset$police)
+
+# police involved gun violence were mostlty at Other unspecified place.
+# in non police involved gun violences were mostly at Home.
+
+
+# Since all the police involved gun violences were Homicide, so keeping homicide category aside they are different 
+# from other intent types.
